725f248ad2eb48439662878e115eca7bb2916f7e
RyuJungSik <xvbc123@gmail.com>
2023-01-23 22:05:06 +0900

    refactor : 클래스명, method로 변경

--- calendar/src/Calendar.java
+++ calendar/src/Calendar.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-
-        //요구 사항 1
+public class Calendar {
+    private static final int[] MAX_DAYS={31,28,31,30,31,30,31,31,30,31,30,12};
+    public void printSampleClender(){
         System.out.println("일 월 화 수 목 금 토");
         System.out.println("--------------------");
         System.out.println("1  2  3  4  5  6  7");
         System.out.println("8  9 10 11 12 13 14");
         System.out.println("15 16 17 18 19 20 21");
         System.out.println("22 23 24 25 26 27 28");
+    }
+
+    public int getMaxDaysOfMonth(int month){
+        return MAX_DAYS[month-1];
+
+    }
+
+    public static void main(String[] args) {
+        Calendar cal=new Calendar();
+        //요구 사항 1
+        cal.printSampleClender();
+
 
         //요구 사항 2
         Scanner scanner=new Scanner(System.in);
@@ -23,8 +34,7 @@ public class Main {
         //요구 사항 3
         System.out.println("달을 입력하세요.");
         int month=scanner.nextInt();
-        int[] maxDays={31,28,31,30,31,30,31,31,30,31,30,12};
-        System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month-1]);
+        System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
 
 
     }
